0a4bcf4d0ed12416eee22054e372029f4ba23f58
Technocurles <sonidhyana916@gmail.com>
2026-04-29 20:11:18 +0530

    fix: bind server to 0.0.0.0

--- backend/app.js
+++ backend/app.js
@@ -50,7 +50,10 @@ mongoose.connect(process.env.MONGO_URI)
 
 
 // Routes
-app.get("/", (req, res) => res.send("Root is Working"));
+// app.get("/", (req, res) => res.send("Root is Working"));
+app.get("/", (req, res) => {
+  res.status(200).send("Backend is LIVE 🚀");
+});
 
 
 // Create HTTP server
@@ -61,4 +64,9 @@ const io = initializeSocket(server);
 app.set("io", io);
 
 // Server
-server.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+// server.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
